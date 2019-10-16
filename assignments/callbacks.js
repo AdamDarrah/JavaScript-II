318eac39c4227dae4aebb437fba43f18876abb83
AdamDarrah <darrah_adam@yahoo.com>
2019-10-17 06:47:39 +0700

    Could not get to closures

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,4 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// . You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -41,24 +41,45 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
+console.log(getLength(items, (lengthOfArray) => lengthOfArray.length));
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.length - 1);
 }
+last(items, (lastItem) => {
+  console.log(lastItem);
+});
+
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+return cb(x, y);
 }
+sumNums(5, 5, function(x, y) {
+  console.log(x + y)
+});
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+return cb(x, y);
 }
+multiplyNums(5, 5, function(x, y){
+  console.log (x * y);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+return cb (item, list);  
 }
+
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,8 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+//function within a function
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
